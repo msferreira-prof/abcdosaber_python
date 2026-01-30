ef9ad6631b6d01f33d58605cba23840233f231d1
Marcos S. Ferreira <32803953+msferreira-prof@users.noreply.github.com>
2026-01-29 21:50:50 -0300

    Model TipoDeAtividade

--- abcdosaber/urls.py
+++ abcdosaber/urls.py
@@ -17,10 +17,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 from django.views.generic import TemplateView
-from django.conf import settings
-from django.conf.urls.static import static
-
-from abcdosaber import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
